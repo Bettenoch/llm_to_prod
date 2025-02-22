156b11bdecf00a60dda38890bd5a3b361566e5a9
Bettenoch <bettenoch254@gmail.com>
2025-02-22 09:59:55 +0300

    load model_tokenizers

---
+++ transformers/attention.py
@@ -0,0 +1,11 @@
+from transformers import AutoModelForCausalLm, AutoTokenizer
+
+
+OPT = AutoModelForCausalLm.from_pretrained("facebook/opt-1.3b", load_in_8bit=True)
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b")
+
+inp = "The quick brown fox jumps over the lazy dog"
+
+inp_tokenized = tokenizer(inp, return_tensors="pt")
+print(inp_tokenized["input_ids"].size())
+print(inp_tokenized)
